43a710913e3354a062734d725a7b8dc3e37b96d2
Ibrahimsuf <110345830+Ibrahimsuf@users.noreply.github.com>
2024-04-12 22:10:04 -0500

    Add ability to upload files

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file part'
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return 'No selected file'
+
+    # Save the file to a folder (in this case, 'uploads' within the 'static' folder)
+    file.save('static/uploads/' + file.filename)
+
+    return 'File uploaded successfully'
 
 
 if __name__ == '__main__':
